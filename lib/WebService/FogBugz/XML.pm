c304af30e1bd784938476f9acb757aac998d69ec
Gareth Kirwan <gbjk@thermeon.com>
2013-08-06 12:37:19 +0100

    Added prompting facility for username and password.

--- lib/WebService/FogBugz/XML.pm
+++ lib/WebService/FogBugz/XML.pm
@@ -5,11 +5,12 @@ use v5.10;
 
 use Config::Any;
 use HTTP::Request;
+use IO::Prompt;
 use LWP::UserAgent;
 use WebService::FogBugz::XML::Case;
 use XML::LibXML;
-use namespace::autoclean;
 
+use namespace::autoclean;
 
 has config_filename => (
     is => 'ro',
@@ -41,12 +42,12 @@ has url => (
 has email => (
     is       => 'ro',
     isa      => 'Str',
-    default  => '',
+    lazy_build  => 1,
     );
 has password => (
     is       => 'ro',
     isa      => 'Str',
-    default  => '',
+    lazy_build  => 1,
     );
 has token => (
     is          => 'rw',
@@ -80,10 +81,16 @@ sub _build_token {
         return $token;
         }
 
-    # TODO: Otherwise, ask the user for password
-    die "TODO: I don't know how to ask you for your password";
+    my $token = $self->logon;
 
-    return;
+    return $token;
+    }
+sub _build_email {
+    return "".prompt "Fogbugz Email address: ", '-t';
+    }
+
+sub _build_password {
+    return "".prompt "Fogbugz Password: ", -te => '*';
     }
 
 sub logon {
@@ -94,7 +101,9 @@ sub logon {
         password    => $self->password,
         });
 
-    $self->token($dom->findvalue('//token'));
+    my $token = $dom->findvalue('//token');
+
+    return $token;
     }
 
 sub logout {
